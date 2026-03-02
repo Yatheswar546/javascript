40afff89dd51fc6f6a8192e8128abf42a7417125
Yatheswar546 <yatheswar546@gmail.com>
2026-03-02 12:37:34 +0530

    numbers-problems

--- 13. js-numbers-math/script.js
+++ 13. js-numbers-math/script.js
@@ -19,3 +19,44 @@ console.log(`Math.max(8,9,0,19) = ${Math.max(8,9,0,19)}`);
 console.log(`Math.min(9,10,18,10,-9,-6) = ${Math.min(9,10,18,10,-9,-6)}`);
 console.log(`Math.pow(4,87) = ${Math.pow(4,87)}`);
 console.log(`Math.PI = ${Math.PI}`);
+
+////////////// Problems //////////////
+
+// Problem 1: Round the number to 2 decimal places
+
+function roundingNumber(){
+    let num = document.getElementById("roundNum").value;
+
+    // let result = Math.round(num * 100) / 100;
+    let result = Number(num).toFixed(2);
+
+    document.getElementById("roundResult").innerHTML = `Result: ${result}`;
+}
+
+// Problem 2: Generate a Random number in given range
+function randomNumber(){
+    let start = document.getElementById("startingNumber").value;
+    let end = document.getElementById("endingNumber").value;
+
+    // Formula:  Math.floor(Math.random() * (max - min)) + min
+
+    let result = Math.floor(Math.random() * (Number(end) - Number(start))) + Number(start);
+
+    document.getElementById("randomResult").innerHTML = `Random Number in given range is: ${result}`;
+
+}
+
+function checkInteger(){
+    let num = document.getElementById("number").value;
+
+    // if(Number.isInteger(Number(num))){
+    //     document.getElementById("result").innerHTML = `Result: It is Integer`;
+    // }
+    // else{
+    //     document.getElementById("result").innerHTML = `Result: Not an Integer`;
+    // }
+
+    // Number.isInteger(Number(num)) ? document.getElementById("result").innerHTML = `Result: It is Integer` : document.getElementById("result").innerHTML = `Result: Not an Integer`;
+
+    document.getElementById("result").innerHTML = Number.isInteger(Number(num)) ? `Result: It is Integer` : `Result: Not an Integer`
+}
