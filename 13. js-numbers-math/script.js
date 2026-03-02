a52a872b7c9b29b402039c802b7d219bbe045055
Yatheswar546 <yatheswar546@gmail.com>
2026-03-02 11:20:35 +0530

    numbers-math

---
+++ 13. js-numbers-math/script.js
@@ -0,0 +1,21 @@
+// Special Numbers in Js
+console.log(`7/0 = ${7/0}`);
+console.log(`-8/0 = ${-8/0}`);
+console.log(`-0/17 = ${-0/17}`);
+console.log(`"Js"/187 = ${"Js"/187}`);
+console.log(`[1,9,8]/0 = ${[1,9,8]/0}`);
+
+// Floating Point Numbers in Js
+console.log(`202.2 * 2 = ${202.2 * 2}`);
+console.log(`303.3 * 3 = ${303.3 * 3}`);
+console.log(`0.1 + 0.2 = ${0.1 + 0.2}`);
+
+// Math Functions in Js
+console.log(`Math.trunc(3.654) = ${Math.trunc(3.654)}`);
+console.log(`Math.round(7.876810) = ${Math.round(7.876810)}`);
+console.log(`Math.floor(9.621793) = ${Math.floor(9.621793)}`);
+console.log(`Math.ceil(5.66849846) = ${Math.ceil(5.66849846)}`);
+console.log(`Math.max(8,9,0,19) = ${Math.max(8,9,0,19)}`);
+console.log(`Math.min(9,10,18,10,-9,-6) = ${Math.min(9,10,18,10,-9,-6)}`);
+console.log(`Math.pow(4,87) = ${Math.pow(4,87)}`);
+console.log(`Math.PI = ${Math.PI}`);
