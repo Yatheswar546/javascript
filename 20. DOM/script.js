8f79f399227136bf59d8601ec47e606d43b9f205
Yatheswar546 <yatheswar546@gmail.com>
2026-04-15 15:46:39 +0530

    DOM-todo-list-2

--- 20. DOM/script.js
+++ 20. DOM/script.js
@@ -17,13 +17,17 @@ function addItem(){
     let itemUpdateBtn = document.createElement('button');
     let itemUpdateText = document.createTextNode('Done');
     itemUpdateBtn.appendChild(itemUpdateText);
-    itemUpdateBtn.onclick = updateItem;
+    itemUpdateBtn.onclick = function(){
+        updateItem(this);
+    };
     // itemUpdateBtn.id = "item" + (taskList.childElementCount) + 1;
 
     let itemDeleteBtn = document.createElement('button');
     let itemDeleteText = document.createTextNode('Delete');
     itemDeleteBtn.appendChild(itemDeleteText);
-    itemDeleteBtn.onclick = deleteItem;
+    itemDeleteBtn.onclick = function(){
+        deleteItem(this);
+    };
     // itemDeleteBtn.id = "item" + (taskList.childElementCount) + 1;
 
     itemDiv.appendChild(itemHeading);
@@ -33,12 +37,17 @@ function addItem(){
     newItem.appendChild(itemDiv);
 
     taskList.appendChild(newItem);
+
+    document.getElementById("inputTask").value = "";
 }
 
-function updateItem(){
-    let item = document.getElementById()
+function updateItem(button){
+    let div = button.parentElement;
+    let heading = div.querySelector("h3");
+    heading.style.textDecoration = "line-through";
 }
 
-function deleteItem(){
-    console.log("Delete");
+function deleteItem(button){
+    let li = button.parentElement.parentElement;
+    li.remove();
 }
